9d1c53af8895d70fff4a9ee8b1def5b0d6c7f0e6
grosroro <grosroro@neutron>
2012-05-31 12:24:26 +0200

    Add API tools and Doc

---
+++ docs/source/API/API/tree.js
@@ -0,0 +1,12 @@
+var tree = [["DataURI","DataURI.html","",
+                [["Exception","DataURI\/Exception.html","",
+                [["Exception","DataURI\/Exception\/Exception.html","",
+                [        ]],["FileNotFoundException","DataURI\/Exception\/FileNotFoundException.html"," < Exception",
+                [        ]],["InvalidArgumentException","DataURI\/Exception\/InvalidArgumentException.html"," < InvalidArgumentException",
+                [        ]],["InvalidDataException","DataURI\/Exception\/InvalidDataException.html"," < Exception",
+                [        ]],["TooLongDataException","DataURI\/Exception\/TooLongDataException.html"," < Exception",
+                [        ]]        ]],["Data","DataURI\/Data.html","",
+                [        ]],["Dumper","DataURI\/Dumper.html","",
+                [        ]],["Parser","DataURI\/Parser.html","",
+                [        ]]        ]]        ]
+
